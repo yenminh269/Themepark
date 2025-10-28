9dd4553d28c416750a45cb51b17b55396a0d5cda
yenminh269 <hhyenminh2001@gmail.com>
2025-10-28 03:25:26 -0500

    'database config'

--- server/config/db.js
+++ server/config/db.js
@@ -7,8 +7,7 @@ const db = mysql.createConnection({
   port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  ssl: { rejectUnauthorized: true } 
+  database: process.env.DB_NAME
 }); 
 db.connect((err) => {
   if (err) return console.error(err.message);
